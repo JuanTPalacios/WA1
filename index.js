e1e4c0012f8f8c2ed796d7b01fdda3e8eaaa7ac1
Juan T. Palacios <juante92@gmail.com>
2024-07-22 14:57:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ const _ = {};
 // Returns the collection for chaining.
 _.each = (collection, iteratee, context) => {
 
+    if (Array.isArray(collection)) {
+   // for (let i of collection) { it's not iterating, why?? not passing the first test..
+   for (let i = 0; i < collection.length; i++ ) {
+      iteratee.call(context, collection[i], i, collection);
+    }
+  } else {
+    for (let key in collection) {
+      collection.hasOwnProperty(key) && iteratee.call(context, collection[key], key, collection);
+    }
+  }
+  return collection;
+
 };
 
 // _.reduce(collection, iteratee, [accumulator], [context])
@@ -29,14 +41,26 @@ _.each = (collection, iteratee, context) => {
 // to the initial invocation of reduce, iteratee is not invoked on the first element,
 // and the first element is instead passed as accumulator for the next invocation.
 _.reduce = (collection, iteratee, accumulator, context) => {
-
+    //Need to loop and do something with every element that the loops iterates to. _.each re-usable?
+  // accumulator === false as condition? but if accumulator gets 0 is falsy but it does exist. Check if accumulator !== (null//undefined)
+  // Checking the accumulator only will happen on the first iteration cause if else gets triggered, accumulator will no longer be null/undefined.
+  _.each(collection, (element, key) => {
+    if (accumulator !== undefined || null) { // ! if the order is null || undefined  should be able to reduce a collection to a single value  NOT PASSES why?
+      accumulator = iteratee.call(context, accumulator, element, key, collection);
+    } else {
+      accumulator = element
+    }
+  })
+  return accumulator;
 };
 
 // _.bind(function, object)
 // Binds a function to an object (obviously without using `bind`), meaning that whenever
 // the function is called, the value of "this" will be the object. Returns the bound function.
 _.bind = function (func, obj) {
-
+    return function () {
+      return func.call(obj, ...arguments);
+    }
 };
 
 // _.memoize(func)
@@ -47,14 +71,21 @@ _.bind = function (func, obj) {
 // will check if it has already computed the result for the given argument
 // and return that value instead of recomputing it.
 _.memoize = func => {
-
+   let cache = {};
+   return function (res) {
+    if (cache.hasOwnProperty(res) || (cache[res] = func(res))){
+      return cache[res];
+    }
+   }
 };
 
 // Now, using the pesudo-classical approach, create a class named
 // "UnderCollections" that has a property named "libraryDesc",
 // and includes the collection methods you just implemented above
 // (i.e. each and reduce).
-
+function UnderCollections() {
+  this.libraryDesc = this.libraryDesc;
+}
 // Then, create a sub-class of "UnderCollections" named
 // "UnderFunctions" that includes the function methods
 // you implemented here above (i.e. bind and memoize).
